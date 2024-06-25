32690f4677d3a6535f24792403c5b78012fdb1a9
Anudeep28 <anupatil28@gmail.com>
2024-06-25 14:14:27 +0530

    commited from desktop changes

--- chitfund/settings.py
+++ chitfund/settings.py
@@ -117,23 +117,21 @@ WSGI_APPLICATION = 'chitfund.wsgi.application'
 # for postgresql functionality
 DATABASES = {
     'default': {
-<<<<<<< HEAD
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': env("DB_USER")+"$"+env("DB_NAME"),#'<your_username>$<your_database_name>',
-        'USER': env("DB_USER"),#'<your_username>',
-        'PASSWORD': env("DB_PASSWORD"),#'<your_mysql_password>',
-        'HOST': env("DB_HOST"),#'<your_mysql_hostname>',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            }
-=======
+        ####### This for production case ####################
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': env("DB_USER")+"$"+env("DB_NAME"),#'<your_username>$<your_database_name>',
+        # 'USER': env("DB_USER"),#'<your_username>',
+        # 'PASSWORD': env("DB_PASSWORD"),#'<your_mysql_password>',
+        # 'HOST': env("DB_HOST"),#'<your_mysql_hostname>',
+        # 'OPTIONS': {
+        #     'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        #     }
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': env("DB_NAME"),#'chitfund',
         'USER': env("DB_USER"),#'postgres',
         'PASSWORD': env("DB_PASSWORD"),#'Onepiece@28',
         'HOST': env("DB_HOST"),#'localhost',
         'PORT': env("DB_PORT"),#'5432',
->>>>>>> 85d3363fc61e3361175fae53437af7452a1dd83f
     }
 }
 
